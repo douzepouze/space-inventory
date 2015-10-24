b383836538d7cd8a686ff549a1e362c0bb73757e
Steffen Goertz <steffen@steffen-goertz.de>
2015-10-24 22:27:23 +0200

    removed some unused code from views.

--- items/views.py
+++ items/views.py
@@ -14,16 +14,12 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q, Count
 
 from django.http.response import Http404
-from django.http import HttpResponseNotFound
 from django.shortcuts import redirect
 from django.utils.html import escape
 from items.forms import InventoryImageUploadForm, LocationImageUploadForm
-# from bootstrap_toolkit.widgets import BootstrapUneditableInput
-
-# from .forms import TestForm, TestModelForm, TestInlineForm, WidgetsForm, FormSetInlineForm
 
 # Local Imports
-from items.models import Inventory, InventoryPhoto, Location, InventoryType
+from items.models import Inventory, Location, InventoryType
 
 
 def make_tree(objects):
@@ -210,75 +206,3 @@ def person_show(request, id):
 
 def inventory_type_show(request, id):
     pass
-
-
-def demo_form_with_template(request):
-    layout = request.GET.get('layout')
-    if not layout:
-        layout = 'vertical'
-    if request.method == 'POST':
-        form = TestForm(request.POST)
-        form.is_valid()
-    else:
-        form = TestForm()
-    modelform = TestModelForm()
-    return render_to_response('form_using_template.html', RequestContext(request, {
-        'form': form,
-        'layout': layout,
-    }))
-
-
-def demo_form_inline(request):
-    layout = request.GET.get('layout', '')
-    if layout != 'search':
-        layout = 'inline'
-    form = TestInlineForm()
-    return render_to_response('form_inline.html', RequestContext(request, {
-        'form': form,
-        'layout': layout,
-    }))
-
-
-def demo_formset(request):
-    layout = request.GET.get('layout')
-    if not layout:
-        layout = 'inline'
-    DemoFormSet = formset_factory(FormSetInlineForm)
-    if request.method == 'POST':
-        formset = DemoFormSet(request.POST, request.FILES)
-        formset.is_valid()
-    else:
-        formset = DemoFormSet()
-    return render_to_response('formset.html', RequestContext(request, {
-        'formset': formset,
-        'layout': layout,
-    }))
-
-
-def demo_tabs(request):
-    layout = request.GET.get('layout')
-    if not layout:
-        layout = 'tabs'
-    tabs = [
-        {
-            'link': "#",
-            'title': 'Tab 1',
-            },
-        {
-            'link': "#",
-            'title': 'Tab 2',
-            }
-    ]
-    return render_to_response('tabs.html', RequestContext(request, {
-        'tabs': tabs,
-        'layout': layout,
-    }))
-
-
-def demo_widgets(request):
-    layout = request.GET.get('layout', 'vertical')
-    form = WidgetsForm()
-    return render_to_response('form.html', RequestContext(request, {
-        'form': form,
-        'layout': layout,
-    }))
